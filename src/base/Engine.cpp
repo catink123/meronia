75631230885f0a46ed8dec552755a2df81f456f4
Catink123 <qwerty.qaz120@gmail.com>
2024-09-22 14:59:59 +0300

    trying out aspect ratio/scaling fixes

--- src/base/Engine.cpp
+++ src/base/Engine.cpp
@@ -1,16 +1,62 @@
 #include "Engine.h"
 
 Engine::Engine() :
-    m_window(sf::VideoMode(640, 480), "Title"),
-    m_entityManager()
+    m_window(sf::VideoMode(640, 480), "Title", sf::Style::Default, sf::ContextSettings(0, 0, 8, 1, 1, 0, false)),
+    m_entityManager(),
+    m_tempShape(sf::Vector2f(100, 100)),
+    m_windowOutline(sf::Vector2f(640 - 10, 480 - 10)),
+    m_lastFrameTime(0),
+    m_prevWindowSize(640, 480)
 {
+    m_tempShape.setOrigin(sf::Vector2f(50, 50));
+    m_tempShape.setPosition(sf::Vector2f(320, 240));
+
+    m_windowOutline.setPosition(sf::Vector2f(5, 5));
+    m_windowOutline.setOutlineThickness(5);
+    m_windowOutline.setOutlineColor(sf::Color::Red);
+    m_windowOutline.setFillColor(sf::Color::Transparent);
 }
 
 void Engine::Run()
 {
     while (m_window.isOpen())
     {
+        sf::Event event;
+        while (m_window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                m_window.close();
+            }
+            if (event.type == sf::Event::Resized)
+            {
+                double newAspectRatio = (double)event.size.width / (double)event.size.height;
 
+                m_window.setView(sf::View(sf::FloatRect(0, 0, (640 / (4 / 3)) * newAspectRatio, 480)));
+            }
+        }
+
+        float elapsedTime = m_frameClock.getElapsedTime().asSeconds();
+        float deltaTime = elapsedTime - m_lastFrameTime;
+        m_lastFrameTime = elapsedTime;
+
+        update(deltaTime);
+        draw();
     }
+}
+
+void Engine::draw()
+{
+    m_window.clear();
+
+    m_window.draw(m_tempShape);
+    m_window.draw(m_windowOutline);
+
+    m_window.display();
+}
+
+void Engine::update(float deltaTime)
+{
+    m_tempShape.rotate(deltaTime * 50);
 }
 
--- src/base/Engine.h
+++ src/base/Engine.h
@@ -2,6 +2,9 @@
 #include <SFML/Graphics/RenderWindow.hpp>
 #include "systems/EntityManager.h"
 #include "Entity.h"
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
 
 class Engine
 {
@@ -11,7 +14,18 @@ public:
     void Run();
 
 private:
+    void draw();
+    void update(float deltaTime);
+
+private:
     sf::RenderWindow m_window;
+    sf::Clock m_frameClock;
+    float m_lastFrameTime;
+
+    sf::RectangleShape m_tempShape;
+    sf::RectangleShape m_windowOutline;
+
+    sf::Vector2u m_prevWindowSize;
 
     EntityManager m_entityManager;
 };
